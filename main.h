f4bf01c9b0fcda01ffa2296e1d30e5dd01b349c5
marwafd <fadil.marwa17@gmail.com>
2023-05-27 03:28:30 -0700

    all tasks

--- main.h
+++ main.h
@@ -40,7 +40,7 @@ struct fmt
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *i,
+int handleprint(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
 /****************** FUNCTIONS ******************/
@@ -93,18 +93,11 @@ int print_string_rot13(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
 /* width handler */
-int handle_write_char(char c, char buffer[],
-	int flags, int width, int precision, int size);
-int write_number(int is_positive, int ind, char buffer[],
-	int flags, int width, int precision, int size);
-int write_num(int ind, char bff[], int flags, int width, int precision,
-	int length, char padd, char extra_c);
-int write_pointer(char buffer[], int ind, int length,
-	int width, int flags, char padd, char extra_c, int padd_start);
-
-int write_unsgnd(int is_negative, int ind,
-char buffer[],
-	int flags, int width, int precision, int size);
+int handle_write(char c, char buffer[], int flags, int width, int precision, int size);
+int write_nmbr(int is_positive, int ind, char buffer[], int flags, int width, int precision, int size);
+int write_numero(int ind, char bff[], int flags, int width, int precision, int length, char padd, char extra_c);
+int writepointer(char buffer[], int ind, int length, int width, int flags, char padd, char extra_c, int padd_start);
+int write_unsigned_num(int is_negative, int ind, char buffer[], int flags, int width, int precision, int size);
 
 /****************** UTILS ******************/
 int is_printable(char);
